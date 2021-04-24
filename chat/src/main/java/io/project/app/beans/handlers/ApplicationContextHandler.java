c1009874368f8459c604102ec49490256ed7207b
armena <armen.arzumanyan@optym.com>
2021-04-24 19:08:28 +0400

    ref

--- chat/src/main/java/io/project/app/beans/handlers/ApplicationContextHandler.java
+++ chat/src/main/java/io/project/app/beans/handlers/ApplicationContextHandler.java
@@ -14,13 +14,8 @@ public class ApplicationContextHandler implements Serializable {
 
     private static final Logger LOGGER = Logger.getLogger(ApplicationContextHandler.class);
     private static final String ACCOUNT_SERVICE = "account/";
-    private static final String DICTIONARY_SERVICE = "dictionary/";
-    private static final String ENCOUNTER_SERVICE = "encounter/";
-    private static final String PRACTITIONER_SERVICE = "practitioner/";
-    private static final String PATIENT_SERVICE = "patient/";
-    private static final String EVENT_SERVICE = "event/";
-    private static final String TELEGRAM_SERVICE = "telegram/";
-
+    private static final String FRIEND_SERVICE = "friend/";
+  
     private String gatewayUrl = this.getBackendPathBundle().getString("gateway");
 
     public ApplicationContextHandler() {
@@ -37,34 +32,16 @@ public class ApplicationContextHandler implements Serializable {
         FacesContext context = FacesContext.getCurrentInstance();
         return context.getApplication().evaluateExpressionGet(context, "#{path}", PropertyResourceBundle.class);
     }
-    
-     public String getTelegramServicePath() {
-        return gatewayUrl + TELEGRAM_SERVICE;
-    }
-
-    public String getEncounterServicePath() {
-        return gatewayUrl + ENCOUNTER_SERVICE;
-    }
-
-    public String getPatientServicePath() {
-        return gatewayUrl + PATIENT_SERVICE;
-    }
-
-    public String getPractitionerServicePath() {
-        return gatewayUrl + PRACTITIONER_SERVICE;
-    }
+   
 
     public String getAccountServicePath() {
         return gatewayUrl + ACCOUNT_SERVICE;
     }
 
-    public String getDictionaryServicePath() {
-        return gatewayUrl + DICTIONARY_SERVICE;
+    public String getFriendServicePath() {
+        return gatewayUrl + FRIEND_SERVICE;
     }
 
-    public String getEventServicePath() {
-        return gatewayUrl + EVENT_SERVICE;
-    }
 
     public String getGatewayUrl() {
         return gatewayUrl;
